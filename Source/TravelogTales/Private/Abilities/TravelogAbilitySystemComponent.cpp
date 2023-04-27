bda195f65d3bf8e3c34888e486573f8551704cfc
CarryCry <1046762149@qq.com>
2023-04-27 16:42:41 +0800

    asc

--- Source/TravelogTales/Private/Abilities/TravelogAbilitySystemComponent.cpp
+++ Source/TravelogTales/Private/Abilities/TravelogAbilitySystemComponent.cpp
@@ -3,4 +3,104 @@
 
 #include "Abilities/TravelogAbilitySystemComponent.h"
 
+#include "GameplayTagsModule.h"
+
+
+bool UTravelogAbilitySystemComponent::GrantAbility(TSubclassOf<UGameplayAbility> Ability)
+{
+	if (!Ability)
+	{
+		return false;
+	}
+
+	FGameplayAbilitySpec Spec(Ability, 1, INDEX_NONE, GetOwner());
+	FGameplayAbilitySpecHandle SpecHandle = GiveAbility(Spec);
+
+	return SpecHandle.IsValid();
+}
+
+bool UTravelogAbilitySystemComponent::RevokeAbility(TSubclassOf<UGameplayAbility> Ability)
+{
+	if (!Ability)
+	{
+		return false;
+	}
+
+	FGameplayAbilitySpecHandle FoundHandle;
+
+	for (const FGameplayAbilitySpec& Spec : GetActivatableAbilities())
+	{
+		if (Spec.Ability->GetClass() == Ability)
+		{
+			FoundHandle = Spec.Handle;
+			break;
+		}
+	}
+
+	if (FoundHandle.IsValid())
+	{
+		ClearAbility(FoundHandle);
+		return true;
+	}
+
+	return false;
+}
+
+bool UTravelogAbilitySystemComponent::HasAbility(TSubclassOf<UGameplayAbility> Ability)
+{
+	if (!Ability)
+	{
+		return false;
+	}
+
+	for (const FGameplayAbilitySpec& Spec : GetActivatableAbilities())
+	{
+		if (Spec.Ability->GetClass() == Ability)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+FActiveGameplayEffectHandle UTravelogAbilitySystemComponent::ApplyGameplayEffect(TSubclassOf<UGameplayEffect> Effect,
+	float Level)
+{
+	if (!Effect)
+	{
+		return FActiveGameplayEffectHandle();
+	}
+
+	UGameplayEffect* GameplayEffect = Effect.GetDefaultObject();
+	FGameplayEffectSpec Spec(GameplayEffect, MakeEffectContext(), Level);
+
+	return ApplyGameplayEffectSpecToSelf(Spec);
+}
+
+bool UTravelogAbilitySystemComponent::RemoveGameplayEffect(FActiveGameplayEffectHandle Handle)
+{
+	if (Handle.IsValid())
+	{
+		RemoveActiveGameplayEffect(Handle);
+		return true;
+	}
+
+	return false;
+}
+
+/*
+void UTravelogAbilitySystemComponent::ActivateAbilitiesWithTags(const FGameplayTagContainer& TagsToActivate,
+	bool bActivate)
+{
+	
+}
+
+void UTravelogAbilitySystemComponent::ActivateAbilityWithTag(const FGameplayTag& TagToActivate, bool bActivate)
+{
+	FGameplayTagContainer TagsToActivate;
+	TagsToActivate.AddTag(TagToActivate);
+	ActivateAbilitiesWithTags(TagsToActivate, bActivate);
+}
+*/
 
--- Source/TravelogTales/Public/Abilities/TravelogAbilitySystemComponent.h
+++ Source/TravelogTales/Public/Abilities/TravelogAbilitySystemComponent.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "AbilitySystemComponent.h"
+#include "GameplayTagsManager.h"
 #include "TravelogAbilitySystemComponent.generated.h"
 
 /**
@@ -14,7 +15,32 @@ class TRAVELOGTALES_API UTravelogAbilitySystemComponent : public UAbilitySystemC
 {
 	GENERATED_BODY()
 
+public:
+	// 为这个ASC授予一个技能
+	UFUNCTION(BlueprintCallable, Category = "Abilities")
+	bool GrantAbility(TSubclassOf<UGameplayAbility> Ability);
 
+	// 从这个ASC撤销一个技能
+	UFUNCTION(BlueprintCallable, Category = "Abilities")
+	bool RevokeAbility(TSubclassOf<UGameplayAbility> Ability);
 
-	
+	// 检查这个ASC是否具有某个技能
+	UFUNCTION(BlueprintCallable, Category = "Abilities")
+	bool HasAbility(TSubclassOf<UGameplayAbility> Ability);
+
+	// 应用游戏效果到这个ASC
+	UFUNCTION(BlueprintCallable, Category = "Abilities")
+	FActiveGameplayEffectHandle ApplyGameplayEffect(TSubclassOf<UGameplayEffect> Effect, float Level);
+
+	// 从这个ASC移除一个游戏效果
+	UFUNCTION(BlueprintCallable, Category = "Abilities")
+	bool RemoveGameplayEffect(FActiveGameplayEffectHandle Handle);
+/*
+	UFUNCTION(BlueprintCallable, Category = "Abilities")
+	void ActivateAbilitiesWithTags(const FGameplayTagContainer& TagsToActivate, bool bActivate);
+
+	UFUNCTION(BlueprintCallable, Category = "Abilities")
+	void ActivateAbilityWithTag(const FGameplayTag& TagToActivate, bool bActivate);
+*/
+
 };
